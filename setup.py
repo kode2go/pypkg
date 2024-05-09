01cdc24d68e92238cc35dc69eb203578a3fa41ee
kode2go <brsbin001@myuct.ac.za>
2024-05-09 17:06:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name='pypkg',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    # Metadata
+    author='Your Name',
+    author_email='your@email.com',
+    description='A simple library for adding numbers',
+    url='https://github.com/kode2go/pypkg',
+)
